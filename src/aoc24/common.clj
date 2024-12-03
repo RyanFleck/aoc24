8855d4dec54f7fcf4576437f98ac2d4cceaa5cd3
Ryan Fleck <Ryan.Fleck@protonmail.com>
2024-12-02 18:40:06 -0700

    Solve namespace loading in tests?

--- src/aoc24/common.clj
+++ src/aoc24/common.clj
@@ -10,15 +10,16 @@
   ;; Hint on setting session here: https://medium.com/@wujido20/advent-of-code-2022-input-loading-made-easy-b38153d6ca2d
 
   ;; Step 1: Make request with token.
-  (let [token (env :aoc-session)
-        url (str "https://adventofcode.com/2024/day/" day "/input")]
-    (println (str "Using token '" (subs token 0 10) "..."))
-    (let [filename (str "resources/day" day ".txt")
-          res (client/get url {:headers {:cookie (str "session=" token)}})]
+  (when (env :aoc-session)
+    (let [token (env :aoc-session)
+          url (str "https://adventofcode.com/2024/day/" day "/input")]
+      (println (str "Using token '" (subs token 0 10) "..."))
+      (let [filename (str "resources/day" day ".txt")
+            res (client/get url {:headers {:cookie (str "session=" token)}})]
 
       ;; Step 2: Write data to correct filename.
-      (spit filename (:body res))
-      (println (str "Wrote body to " filename)))))
+        (spit filename (:body res))
+        (println (str "Wrote body to " filename))))))
 
 ;; Example:
 ;; (download-puzzle-data 1)
