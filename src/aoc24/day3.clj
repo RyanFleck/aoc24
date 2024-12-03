d4629f63f17c5392944e97b3d5c6d9d89d63df59
Ryan Fleck <Ryan.Fleck@protonmail.com>
2024-12-02 22:18:26 -0700

    Solve Day 3 part 1

--- src/aoc24/day3.clj
+++ src/aoc24/day3.clj
@@ -4,9 +4,21 @@
             [clojure.java.io :as io]
             [aoc24.common :as common]))
 
+;; https://adventofcode.com/2024/day/3
+
+(defn str->int [str] (Integer/parseInt str))
+
 (comment
   ;; Fetch day 3's input data:
-  (def input (common/get-input-data 3)))
+  (def input (common/get-input-data 3))
+
+  ;; Answer - Part One
+  ;; One liner baby B)
+  (apply + (map #(* (str->int (nth % 1)) (str->int (nth % 2))) 
+                (re-seq #"mul\(([\d][\d]?[\d]?),([\d][\d]?[\d]?)\)" input))) ; correct!
+
+  ;; Part 2 can wait until tomorrow!
+  )
 
 (defn add [& rest]
   (apply + rest))
