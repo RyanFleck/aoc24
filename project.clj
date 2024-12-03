0124bff552172de832e4cc69b57088a4311f9423
Ryan Fleck <Ryan.Fleck@protonmail.com>
2024-12-02 21:43:20 -0700

    Attempt to implement code coverage

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [org.slf4j/slf4j-nop "2.0.16"]
                  [clj-http "3.13.0"]
                  [environ "1.2.0"]]
-  :plugins [[dev.weavejester/lein-cljfmt "0.13.0"]
+  :plugins [[dev.weavejester/lein-cljfmt "0.13.0"] ; 'lein cljfmt fix' to format repository
+            [lein-cloverage "1.2.4"] ; 'lein cloverage' to get code coverage
             [lein-environ "1.2.0"]]
   :repl-options {:init-ns aoc24.core})
